fbfb175afca3f8ee289b8b79059a0a63c6335cec
geert36 <77209691+geert36@users.noreply.github.com>
2024-12-22 11:55:59 +0100

    Update const.py

--- custom_components/postnl/const.py
+++ custom_components/postnl/const.py
@@ -6,8 +6,6 @@ POSTNL_AUTH_URL = "https://login.postnl.nl/101112a0-4a0f-4bbb-8176-2f1b2d370d7c/
 POSTNL_TOKEN_URL = "https://login.postnl.nl/101112a0-4a0f-4bbb-8176-2f1b2d370d7c/login/token"
 POSTNL_REDIRECT_URI = "postnl://login"
 POSTNL_SCOPE = "profile openid email address phone poa-profiles-api"
-DELIVERY = "delivery"
-DISTRIBUTION = "distribution"
 
 PLATFORMS = [
     Platform.SENSOR
